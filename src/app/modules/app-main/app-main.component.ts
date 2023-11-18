fec9e933c6ac4419ecb71b9b7c51ef577d63c010
Jak Campos <jak.ce30@gmail.com>
2023-11-18 11:16:26 -0500

    first

---
+++ src/app/modules/app-main/app-main.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-app-main',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app-main.component.html',
+  styleUrl: './app-main.component.css'
+})
+export class AppMainComponent {
+
+}
